f5b8f7228b86148723914f6011fc99a6a694022d
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-01-22 16:22:30 +0800

    Update 信号屏蔽.c

--- signal/信号屏蔽.c
+++ signal/信号屏蔽.c
@@ -3,6 +3,25 @@
 #include <signal.h>
 #include <stdlib.h>
 
+
+void show_pending(sigset_t* pending)
+{
+    printf("cur process pending set : ");
+    for (int i = 1; i <= 31; i++)
+    {
+        if (sigismember(pending, i))
+        {
+            printf("1");
+        }
+        else 
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
+
 int main()
 {
     // sigset_t是一个位图结构，但是不同的OS的操作是不同的
@@ -19,10 +38,23 @@ int main()
     sigaddset(&inset, 2);
     // 添加到位图中，并且获得当前进程老的屏蔽字
     sigprocmask(SIG_SETMASK, &inset, &outset);
+    sigset_t pending;
+    int count = 0;
     while (1)
     {
-        printf("hello zyq\n");
+        // 清空位图
+        sigemptyset(&pending);
+        // 获取当前pending位图
+        sigpending(&pending);
+        // 打印pending位图
+        show_pending(&pending);
         sleep(1);
+        count++;
+        if (count == 20)
+        {
+            sigprocmask(SIG_SETMASK, &outset, NULL);
+            printf("对2号信号的屏蔽已经恢复");
+        }
     }
     return 0;
 }

ad7c755a553ecd164747c56838a88e8a8ff51551
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-01-23 13:16:59 +0800

    Create thread_join.c

---
+++ pthread/thread_join.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void* thread_run(void* args)
+{
+    int num = *(int*)args;
+    while (1)
+    {
+        printf("我是新线程%d, 我的id是%u\n", num, pthread_self());
+        sleep(10);
+        break;
+    }
+    return (void*)111;
+}
+
+#define NUM 1
+
+int main()
+{
+    // 创建多个线程
+    pthread_t tid[NUM];
+    for (int i = 0; i < NUM; i++)
+    {
+        pthread_create(&tid[i], NULL, thread_run, (void*)&i);
+        sleep(1);
+    }
+    // 等待tid[0]线程退出
+    void* status = NULL;
+    pthread_join(tid[0], &status);
+    printf("ret : %d\n", *(int*)status);
+    return 0;
+}
